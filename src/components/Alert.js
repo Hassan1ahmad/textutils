2658206dfe6c4e58ae0c77c2a9d7c5ff306fc5f2
Hassan Ahmad <hassan1ahmad999@gmail.com>
2023-09-02 11:52:41 +0500

    update build for gh-pages

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function Alert(props) {
   return (
-    props.alert && 
+    <div style={{height: '50px'}}>
+    {props.alert && 
        <div className={`alert alert-fixed-top  alert-${props.alert.type}`} role="alert">
         {props.alert.message}
+      </div>}
       </div>
 
    
